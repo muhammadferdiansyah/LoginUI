b37844d56a5506beb26c2076fb09095d98658386
muhammadferdiansyah <dandyferdiansyah2801@gmail.com>
2021-02-03 11:36:30 +0700

    first commit

--- LoginUI/ContentView.swift
+++ LoginUI/ContentView.swift
@@ -9,8 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack{
+            Image("co_nf")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                HandleView()
+                FormBox()
+            }
+        }
+    }
+}
+
+struct HandleView: View {
+    var body: some View{
+        VStack{
+            Image(systemName: "hand.draw.fill")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .background(Color.black)
+                .foregroundColor(Color.orange)
+                .padding()
+                .background(Color.black)
+                .cornerRadius(20)
+            
+            Text("Hello Swift").foregroundColor(Color.white)
+        }
+    }
+}
+
+struct FormBox: View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
+    var body: some View{
+        VStack(alignment: .leading){
+            Text("Username").font(.callout).bold()
+            TextField("Username", text: $username)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Text("Password").font(.callout).bold()
+            SecureField("Password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button(action: {print("Hello button")}) {
+                Text("Button")
+            }
+            
             .padding()
+            .background(Color.black)
+            .cornerRadius(10)
+            .foregroundColor(Color.white)
+        }
+        .padding(.all, 20)
+        .background(Color.orange)
+        .cornerRadius(10)
     }
 }
 
